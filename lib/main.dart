31ccfc66066294d2cc5ad6a797153ead6cd2dab7
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2021-05-02 00:27:02 +0100

    https://github.com/HunckleJeremy/PortfolioApp-Zuri-.git

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PortfolioApp());
+}
+class PortfolioApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black26,
+          title: Text("My Portfolio",
+          textDirection: TextDirection.ltr,
+          ),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                  ),
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Text(
+                    "Jeremiah Ekanem",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.amberAccent,
+                  ),
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(20),
+                  child: Text(
+                    "My name is Jeremiah Ekanem, a student learning Mobile Development in the ongoing zuri internship 2021",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                )
+              ),
+            ],
+
+          ),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
